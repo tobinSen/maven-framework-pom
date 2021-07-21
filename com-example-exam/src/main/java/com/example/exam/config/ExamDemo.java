7db4561045d10cdd75e78bc4f6a99eb98a40c3ea
童健 <tongjian2@sfmail.sf-express.com>
2021-07-21 15:56:51 +0800

    init

---
+++ com-example-exam/src/main/java/com/example/exam/config/ExamDemo.java
@@ -0,0 +1,14 @@
+package com.example.exam.config;
+
+import com.google.common.collect.ComparisonChain;
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class ExamDemo {
+
+    public static void main(String[] args) {
+        int result = ComparisonChain.start().compare(1, 4).result();
+        System.out.println("result: " + result);
+
+        System.out.println("random: " + RandomStringUtils.randomNumeric(4));
+    }
+}
