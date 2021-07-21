7d58883b4a408f0db1da12e7343a616b9e2cfa00
tongjian <1245007716@qq.com>
2021-07-21 23:56:45 +0800

    init

---
+++ com-example-service/src/main/java/com/example/service/config/ServiceDemo.java
@@ -0,0 +1,10 @@
+package com.example.service.config;
+
+import com.example.exam.config.ExamDemo;
+
+public class ServiceDemo {
+
+    public static void main(String[] args) {
+        ExamDemo.main(null);
+    }
+}
